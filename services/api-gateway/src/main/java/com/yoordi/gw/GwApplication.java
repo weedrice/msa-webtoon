8fee9668f00427e934568a25ef34a3877dd95e00
weedrice <supp0rtyoo@gmail.com>
2025-08-28 17:02:43 +0900

    Init

---
+++ services/api-gateway/src/main/java/com/yoordi/gw/GwApplication.java
@@ -0,0 +1,8 @@
+package com.example.gw;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwApplication {
+    public static void main(String[] args) { SpringApplication.run(GwApplication.class, args); }
+}
